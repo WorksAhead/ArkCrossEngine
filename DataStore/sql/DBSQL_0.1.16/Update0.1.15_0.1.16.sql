2cfd78e4d19174928ccc767682ce0121daa116e0
winslylord <winslylord@me.com>
2017-12-19 10:36:30 +0800

    添加User最后位置存储过程

---
+++ DataStore/sql/DBSQL_0.1.16/Update0.1.15_0.1.16.sql
@@ -0,0 +1,49 @@
+#
+# Update SQL
+#
+#author: Sirius
+#
+#version notes:
+#  0.1.0
+#    Initial version. Date:2014-09-09
+#  0.1.1
+#    玩家数据增加伙伴PartnerInfo和好友FriendInfo. Date:2014-09-10
+#  0.1.2
+#    邮件表MailInfo中的Text字段大小设为4096字节，数据类型改为text。 Date:2014-09-10
+#  0.1.3
+#    X魂表XSoulInfo中增加一列XSoulModelLevel。 Date:2014-09-12
+#  0.1.4
+#    Account表增加IsValid;UserInfoExtra表增加AttemptAward相关;ExpeditionInfo表增加Rage. Date:2014-09-17
+#  0.1.5
+#    增加数据库版本验证机制。 Date:2014-09-18
+#  0.1.6
+#    完善版本验证机制；数据库表字符串类型修改；存储商品兑换数据。 Date:2014-09-20
+#  0.1.7
+#    存储精力值、每日通关数据和签到数据。 Date:2014-09-25
+#  0.1.8
+#    Account表添加封停标识，新手教学、月卡相关数据，礼包码 Date:2014-09-26
+#  0.1.9
+#    商店刷新计数，刷金副本计数，竞技场数据 Date:2014-10-08
+#  0.1.10
+#    Guid存储修改， Date:2014-10-09
+#  0.1.11
+#    竞技场战斗记录，竞技场排行榜修改，UserInfo，UserInfoExtra，LegacyInfo表修改 Date:2014-10-10
+#  0.1.12
+#    竞技场排行榜修改 Date:2014-10-11
+#  0.1.13
+#    竞技场排行榜修改 Date:2014-10-11
+#  0.1.14
+#    竞技场战斗记录修改 Date:2014-10-16
+#  0.1.15
+#    七天登录数据 Date:2014-10-16
+#  0.1.16
+#    用户场景中最后位移
+
+use dsnode;
+# 修改数据库版本号
+call SetDSNodeVersion('0.1.16');
+
+alter table UserInfo add column X bigint not null;
+update UserInfo set X = '0' where Guid > 0;
+alter table UserInfo add column Z bigint not null;
+update UserInfo set Z = '0' where Guid > 0;
